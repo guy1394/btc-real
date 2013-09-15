24b2e0f6c75be349d24a13a0db5a21ff00f3b827
Kirill Berezin <enelar@develop-project.ru>
2013-09-16 00:01:45 +0400

    Making the qiwi bill

--- btce.php
+++ btce.php
@@ -23,13 +23,8 @@ class btce
     $email = $config->btce_user;
     $password = $config->btce_password;
 
-    $request = new request('https://btc-e.com/ajax/login.php');
-    $request->cookie = $this->cookie;
     $arg = array("email" => $email, "password" => $password, "otp" => $otp, "PoW_nonce" => $pow);
-    $res = $request->Post($arg)->Result();
-    $this->cookie = $request->cookie;
-    var_dump($res);
-    return json_decode($res, true);
+    return $this->BTCERequest('https://btc-e.com/ajax/login.php', $arg);
   }
   
   public function Login()
@@ -37,6 +32,39 @@ class btce
     $obj = $this->tryLogin();
     $pow = $this->getPoW($obj['data']['work']['target'], $obj['data']['work']['data']);
     $obj = $this->tryLogin($pow);
+    assert($obj['data']['login_success'] == 1);
+    return true;
+  }
+  
+  private function BTCERequest( $url, $post )
+  {
+    $request = new request($url);
+    $request->cookie = $this->cookie;
+    $res = $request->Post($post)->Result();
+    $this->cookie = $request->cookie;
+    return json_decode($res, true);  
+  }
+  
+  public function OpenQiwiBill( $number, $amount )
+  {
+    $arg = array("amount" => $amount, "to" => $number);
+    $c = $this->BTCERequest('https://btc-e.com/ajax/qiwi.php', $arg);
+
+    global $config;
+    $params = array(
+      "txn_id" => $c['txn_id'],
+      "from" => $c['from'],
+      "to" => $number,
+      "summ" => $amount,
+      "com" => $c['com'],
+      "lifetime" => 72,
+      "check_agt" => "false",
+      "successUrl" => $config->qiwi_success_url,
+      "failUrl" => $config->qiwi_fail_url,
+      );
+    $redirect = "http://w.qiwi.ru/setInetBill.do?".http_build_query($params);
+    var_dump($params);
+    var_dump($redirect);
   }
 
   function getPoW($a, $b)
@@ -54,3 +82,4 @@ class btce
 
 $a = new  btce();
 $a->Login();
+$a->OpenQiwiBill("9213243303", 10);
