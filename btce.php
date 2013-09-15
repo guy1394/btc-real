8eab1e2a6a1868398771d6f0992609e84091e7ab
Kirill Berezin <enelar@develop-project.ru>
2013-09-15 23:27:16 +0400

    Fixing

--- btce.php
+++ btce.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once('request.php');
+include_once('config.php');
 
 error_reporting(E_ALL); ini_set('display_errors','On'); ini_set('display_startup_errors','On');
 class btce
@@ -18,11 +19,16 @@ class btce
     if ($pow === null)
       $pow = '';
 
+    global $config;
+    $email = $config->btce_user;
+    $password = $config->btce_password;
+
     $request = new request('https://btc-e.com/ajax/login.php');
     $request->cookie = $this->cookie;
     $arg = array("email" => $email, "password" => $password, "otp" => $otp, "PoW_nonce" => $pow);
     $res = $request->Post($arg)->Result();
     $this->cookie = $request->cookie;
+    var_dump($res);
     return json_decode($res, true);
   }
   
