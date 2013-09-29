9c48e34f012b500d8b67829c3fff5a429e36f31f
Kirill Berezin <enelar@develop-project.ru>
2013-09-29 10:50:27 +0400

    Simple phoxy using

---
+++ api/main.php
@@ -0,0 +1,9 @@
+<?php
+
+class main extends api
+{
+  protected function Reserve()
+  {
+    return array("error" => "Coming soon");
+  }
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1')
+  define("_ip_", $_SERVER['REMOTE_ADDR']);
+else
+  define("_ip_", explode(',', $_SERVER["HTTP_X_FORWARDED_FOR"])[0]);
+
+//error_reporting(E_ALL); ini_set('display_errors', '1');
+function phoxy_conf()
+{
+  $ret = phoxy_default_conf();
+  $ret['site'] = 'http://btc-real.com/';
+  return $ret;
+}
+
+include('connect.php');
+include('phoxy/index.php');
